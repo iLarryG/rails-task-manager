6c3b102dec2009822152d5108d14a7184c7fbf19
Larry Grutman <larry.grutman@gmail.com>
2016-08-09 11:30:37 +0200

    Add TasksController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'tasks_controller/index'
+
+  get 'tasks_controller/show'
+
+  get 'tasks_controller/new'
+
+  get 'tasks_controller/create'
+
+  get 'tasks_controller/edit'
+
+  get 'tasks_controller/update'
+
+  get 'tasks_controller/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
